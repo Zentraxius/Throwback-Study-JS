905c0b762226c669bb1554b326e3bb0d915b8da4
Cody Fritz <clanalia55@gmail.com>
2020-09-19 16:00:32 -0700

    save notes after including map method

--- main.js
+++ main.js
@@ -52,5 +52,58 @@ const retailCompanies = companies.filter(function (company) {
     // company is just the parameter put into the function, .category is the "category: Retail" listed in the object. and 'Retail' is matching the string to the object to determine True/False
   } // Objects in this instance are the Companies, or {Objects} inside the Array "[Array]", [{Object1}, {Object2}]
 });
+/////
+//////
+const retailCompanies = companies.filter(company => company.category === 'Retail');
+console.log(retailCompanies) // Same function as the above filter method, but using Filter *and* Arrow functions
+//////
+///////
+const eightiesCompanies = companies.filter(company => (company.start >=1980 && company.start < 1990));
+console.log(eightiesCompanies) // Returns the companies that were started in the 1980s
+///////
+////////
+const lastedTenYears = companies.filter(company => (company.end - company.start >= 10));
+console.log(lastedTenYears); // Returns companies that lasted ten years or more
+////////
+//////////////////////////////////////////////
 
+
+
+//////////////////////////////////////////////
+// map
+const companyNames = companies.map(function(company) {
+return company.name;
+}); // Returns new array with only the company names
+console.log(companyNames)
+//
+///
+const testMap = companies.map(function(company) {
+  return `${company.name} [${company.start} - ${company.end}]`;
+}) // Returns company name and its start - end dates
+console.log(testMap)
+///
+//// Shortened version using arrow functions
+const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+console.log(testMap)
+////
+/////
+const agesSquare = ages.map(age => Math.sqrt(age));
+const agesTimesTwo = ages.map(age=>age*2); // Doubles the age
+
+const ageSquareDouble = ages
+.map(age => Math.sqrt(age))
+.map(age => age*2);
+
+console.log(agesSquare); // gets square root of ages
+console.log(agesTimesTwo); // gets ages * 2
+console.log(ageSquareDouble); // gets ages squared then doubled
+/////
+//////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////
+// sort
+
+//
 //////////////////////////////////////////////
